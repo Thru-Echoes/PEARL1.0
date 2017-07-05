bd663589e6a5ee1d21ed33ea478cb127a5990979
jin00105 <hy324@cornell.edu>
2017-07-05 00:59:09 -0700

    raster_translation_18proj.py creates tile maps for 19 different models for 4 prototype species.

--- raster_data_prep/raster_translation_18proj.py
+++ raster_data_prep/raster_translation_18proj.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 import rpy2.robjects as ro
 from multiprocessing import Pool, cpu_count
 
@@ -24,39 +24,36 @@ def tif_translate(in_dir, out_dir, file):
             writeRaster(r, paste(out_dir, out_file, sep=''), format="GTiff")''' % (in_dir, out_dir, file))
 
 def raster_out (filename):
-
-    _tif_dir = wd + '/pearl_data/tiff/'
-    _tif_dir_c = wd + '/pearl_data/color_tiff/'
-    _color_dir = wd + '/pearl_data/colors/'
-    _tile_dir = wd + '/pearl_data/tilemaps/'
+    print 'making wd\'s'
+    _tif_dir = wd + '/pearl_data_18futures/tiff/'
+    _tif_dir_c = wd + '/pearl_data_18futures/color_tiff/'
+    _color_dir = wd + '/pearl_data_18futures/colors/'
+    _tile_dir = wd + '/pearl_data_18futures/tilemaps/'
 
     if isinstance(filename, list):
 
         for i in range(0, len(filename)):
             raster_out(filename[i])
     else:
-
+        print filename[1]
         _in_file = _tif_dir + filename[0] + filename[1]
-
-        if filename[1].endswith('current.tif'):
-            _color_file = _color_dir + 'color_1.txt'
-            _tile_dir = _tile_dir + filename[0] + '/current'
-        elif filename[1].endswith('ac45.tif'):
-            _color_file = _color_dir + 'color_2.txt'
-            _tile_dir = _tile_dir + filename[0] + '/futureac45'
-        elif filename[1].endswith('ac85.tif'):
-            _color_file = _color_dir + 'color_3.txt'
-            _tile_dir = _tile_dir + filename[0] + '/futureac85'
-        elif filename[1].endswith('bc26.tif'):
-            _color_file = _color_dir + 'color_4.txt'
-            _tile_dir = _tile_dir + filename[0] + '/futurebc26'
-
+        print 'pattern'
+        pattern = '_[a-z]\w+\.'
+        print 'modelName'
+        modelName = re.findall(pattern,filename[1])[0][1:-1]
+        print 'modelIndex'
+        modelIndex = models.index(modelName)
+        print 'make color file'
+        _color_file = _color_dir + 'color_%s.txt'%(modelIndex+1)
+        print 'make tile dir'
+        _tile_dir = _tile_dir + filename[0] + '/' + modelName
+        print 'make out file'
         _out_file = _tif_dir_c + filename[0] + filename[1]
-
-
+        print _out_file
+        print 'doing color_tiff_cmd'
         color_tiff_cmd = 'gdaldem color-relief -of GTiff %s %s %s -alpha' % (_in_file, _color_file, _out_file)
         os.system(color_tiff_cmd)
-
+        print 'doing tile_cmd'
         tile_cmd = 'gdal2tiles.py -z 1-6 %s %s' % (_out_file, _tile_dir)
         os.system(tile_cmd)
 
@@ -70,11 +67,11 @@ def color_file_create(color_dir, color, number):
 print('Creating diretories')
 
 wd = os.getcwd()
-origin = wd + '/pearl_data/origin/'
-tif_dir = wd + '/pearl_data/tiff/'
-tif_dir_c = wd + '/pearl_data/color_tiff/'
-color_dir = wd + '/pearl_data/colors/'
-tile_dir = wd + '/pearl_data/tilemaps/'
+origin = wd + '/pearl_data_18futures/origin/'
+tif_dir = wd + '/pearl_data_18futures/tiff/'
+tif_dir_c = wd + '/pearl_data_18futures/color_tiff/'
+color_dir = wd + '/pearl_data_18futures/colors/'
+tile_dir = wd + '/pearl_data_18futures/tilemaps/'
 
 if not os.path.exists(tif_dir):
     os.makedirs(tif_dir)
@@ -85,18 +82,23 @@ if not os.path.exists(color_dir):
 if not os.path.exists(tile_dir):
     os.makedirs(tile_dir)
 
-#reference http://www.rapidtables.com/web/color/RGB_Color.htm
-color = ['0 0 255','255 0 0','255 128 0','255 255 0',
-'128 255 0','0 255 0', '0 255 128','0 255 255','0 128 255','0 0 255','127 0 255',
-'255 0 255','255 0 127','128 128 128','255 153 153','153 204 255','204 153 255',
-'255 255 153']
+#color reference: http://www.rapidtables.com/web/color/RGB_Color.htm
+color = ['0 0 255','255 0 0','255 128 0','255 255 0','128 255 0','0 255 0',
+ '0 255 128','0 255 255','0 128 255','0 0 255','127 0 255','255 0 255',
+ '255 0 127','128 128 128','255 153 153','153 204 255','204 153 255',
+'255 255 153','255 204 204']
 
-for i in range(0, 4):
+models = ['current','futureac45','futureac85','futurebc26','futurebc45',
+'futurebc60','futurebc85','futurecc26','futurecc45','futurecc60','futurecc85',
+'futurehd26','futurehd45','futurehd60','futurehd85','futurehe26','futurehe45',
+'futurehe60','futurehe85']
+
+for i in range(0, len(color)):
     color_file_create(color_dir, color[i], i+1)
 
 print('Creating GeoTIFF from R raster...')
 
-for folder in os.listdir(wd + '/pearl_data/origin/'):
+for folder in os.listdir(wd + '/pearl_data_18futures/origin/'):
     if not folder.startswith('.') and not folder.endswith('.csv'):
 
         in_dir = origin + folder + '/'
